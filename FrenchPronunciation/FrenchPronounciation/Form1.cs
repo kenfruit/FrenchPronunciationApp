8668f44be6fd4673b37b503e677711d4ccf6ac4b
Ken Fruit <ken.fruit@gmail.com>
2017-07-18 06:59:46 -0500

    Create a new repository for the FrenchPronunciation Project

---
+++ FrenchPronunciation/FrenchPronounciation/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrenchPronunciation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "http://www.collinsdictionary.com/dictionary/french-english/" +
+                textBox1.Text;
+            webBrowser1.Navigate(new Uri(url));
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string done = "DONE";
+        }
+    }
+}
